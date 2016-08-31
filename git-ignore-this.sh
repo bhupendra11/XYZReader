12bbb816838fb85ec25927e870eb6bcb0eb7d64b
Bhupendra Shekhawat <bhupendrashekhawat1108@gmail.com>
2016-09-01 04:17:25 +0530

    Refactored code , removed extra files

---
+++ git-ignore-this.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+git rm -f  --cached build/
+git rm -f --cached build/intermediates
+git rm -f --cached build/intermediates/dex-cache
+git rm -f --cached build/intermediates/dex-cache/cache.xml
+git rm -f --cached build/generated
+git rm -f --cached build/generated/mockable-android-23.jar
